dab3f8d2fab084d81ac4a93bf889c464d0a0f1ff
stadnikivan <39565192+stadnikivan@users.noreply.github.com>
2019-11-08 19:45:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,16 +17,3 @@ function type(str) {
 type();
 //3
 
-
-
-
-
-
-
-
-var arr = [];
-for ( i = 0; i < 7; i++ ) {
-    arr.push( Math.round( Math.random() * 100 ));
-if (50 < arr[i])
-    alert("i = "+arr[i]);
-} console.log(arr);
